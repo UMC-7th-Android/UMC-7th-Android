7feacad81b29d3b3786d6a5b86aafe13b6061cfe
jiwon <pjw99032986@gmail.com>
2024-10-01 17:32:33 +0900

    init : 초기 생성

---
+++ Loopy/Loopy/app/src/main/java/com/example/loopy/MainActivity2.kt
@@ -0,0 +1,29 @@
+package com.example.loopy
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val iv = findViewById<ImageView>(R.id.iv)
+
+        iv.setImageResource(intent.getIntExtra("image",0))
+
+        val tv = findViewById<TextView>(R.id.tv)
+        tv.text = intent.getStringExtra("title")
+    }
+}
